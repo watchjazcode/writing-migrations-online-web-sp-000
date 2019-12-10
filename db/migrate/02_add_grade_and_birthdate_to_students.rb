6f43af9a2a5bc83aaf5e8315e344575ed431c7c6
Jazmin M <jazminmerckel@gmail.com>
2019-12-10 03:40:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :grade
+    add_column :students, :grade, :string
+    add_column :students, :birthdate, :string
   end
 end
